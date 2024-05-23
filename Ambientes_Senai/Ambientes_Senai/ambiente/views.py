9462f94feeb683199b269e69e30501072ece7e64
THAYSZ <thainacristina.sz07@gmail.com>
2024-05-23 11:09:50 -0300

    Criação das rotas, classes e views

--- Ambientes_Senai/Ambientes_Senai/ambiente/views.py
+++ Ambientes_Senai/Ambientes_Senai/ambiente/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Senai  
 # Create your views here.
+def login(request):
+    context = {}
+    dados_senai = Senai.objects.all() #consulta no banco de dados trazendo todas as informações do hotel por meio de um objeto
+    context["dados_senai"] = dados_senai
+    return render(request, 'login.html', context)
+
+def cadastro(request):
+    pass
