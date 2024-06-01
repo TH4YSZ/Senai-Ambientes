b02a4a613b23edfac76132cfb05fd81c9d0ef4be
TH4YSZ <thainacristina.sz07@gmail.com>
2024-06-01 11:12:42 -0300

    CRUD para todos os models completo (Senai, Ambiente, Reserva e Usuário).

--- Ambientes_Senai/api_rest/urls.py
+++ Ambientes_Senai/api_rest/urls.py
@@ -1,10 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('senai', views.senai, name='senai'),
     path('ambiente/', views.ambientes_list_create, name='ambientes_list_create'),
-    path('ambiente/<int:id>', views.ambientes_detail, name='ambiente_detail')
-] 
+    path('ambiente/<int:id>', views.ambientes_detail, name='ambiente_detail'),
+    path('usuario/', views.usuario_create, name='usuario_create'),
+    path('reserva/', views.reservas_list_create, name='reservas_list_create'),
+    path('reserva/<int:id>', views.reservas_detail, name='reserva_detail'),
+]
--- Ambientes_Senai/api_rest/views.py
+++ Ambientes_Senai/api_rest/views.py
@@ -11,6 +11,8 @@ from .serializers import *
 import json
 
 # Create your views here.
+
+# SENAI
 @api_view(['GET'])
 def senai(request):
     if request.method == 'GET':
@@ -23,6 +25,7 @@ def senai(request):
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
+# AMBIENTE
 @api_view(['GET', 'POST'])
 def ambientes_list_create(request):
     if request.method == 'GET':
@@ -78,4 +81,55 @@ def ambientes_detail(request, id):
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
             
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+# USUÁRIO
+@api_view(['POST'])
+def usuario_create(request):
+    if request.method == 'POST':
+        new_usuario = request.data
+        serializer = UsuarioSerializer(data=new_usuario)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+# RESERVA
+@api_view(['GET', 'POST'])
+def reservas_list_create(request):
+    if request.method == 'GET':
+        try:
+            reservas = Reserva.objects.all()
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ReservaSerializer(reservas, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        new_reserva = request.data
+        serializer = ReservaSerializer(data=new_reserva)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def reservas_detail(request, id):
+    try:
+        reserva = Reserva.objects.get(pk=id)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'DELETE':
+        try:
+            reserva.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
     return Response(status=status.HTTP_400_BAD_REQUEST)
